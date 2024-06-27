2c3eaec68c38c8656288c02276bd01411c070bbe
SerobSayadyan <serobsayadyan97@gmail.com>
2024-06-27 18:55:32 +0400

    Innitial Commit

--- Consumer-Service/src/main/java/org/example/consumer_service/ConsumerServiceApplication.java
+++ Consumer-Service/src/main/java/org/example/consumer_service/ConsumerServiceApplication.java
@@ -8,8 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConsumerServiceApplication {
 
     public static void main(String[] args) {
-        DatabaseInitializer.createDatabaseIfNotExists();
-        SpringApplication.run(ConsumerServiceApplication.class, args);
+        try {
+            //This time is needed so that postgresSQL will be created in docker
+            new Thread(() -> {
+                DatabaseInitializer.createDatabaseIfNotExists();
+                SpringApplication.run(ConsumerServiceApplication.class, args);
+            }).wait(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
